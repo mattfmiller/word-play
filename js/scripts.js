9ca02f53b6a6fe369817bfa05be8b48c2d402f2b
Cory West-Forbes and Matt Miller <cory@>
2018-03-26 13:22:03 -0700

    Create print new sentence function to print html text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(function(){
- $("#wordForm").submit(function(event){
+  $("#wordForm").submit(function(event){
 
-   function createNewSentence(){
-    var sentenceInput = $("#sentence-input").val();
-    var wordArray = sentenceToArray(sentenceInput);
-    var greaterThanThree = wordsGreaterThanThree(wordArray);
-    var transformedSentence = reversedSentence(greaterThanThree);
-    console.log(transformedSentence);
-   }
+    function createNewSentence(){
+      var sentenceInput = $("#sentence-input").val();
+      var wordArray = sentenceToArray(sentenceInput);
+      var greaterThanThree = wordsGreaterThanThree(wordArray);
+      var transformedSentence = reversedSentence(greaterThanThree);
+      return transformedSentence;
+    }
 
-   function wordsGreaterThanThree(array) {
+    function wordsGreaterThanThree(array) {
      return array.filter(function(word){
        return word.length > 3;
      });
-   }
+    }
 
     function reversedSentence(arrayOfWords){
       return arrayOfWords.reverse().join(' ');
@@ -22,8 +22,13 @@ $(function(){
     function sentenceToArray(string){
       return string.split(" ");
     }
-    
-   createNewSentence();
-   event.preventDefault();
- });
+
+    function printNewSentence() {
+      $("#newSentence").text(createNewSentence());
+    }
+
+    printNewSentence();
+
+    event.preventDefault();
+  });
 });
